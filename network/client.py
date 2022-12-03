693d0c05e48f4e9b01101a0238f6adafe2dc408e
eugene <minotavr1712@gmail.com>
2022-12-03 18:24:58 +0300

    .

--- network/client.py
+++ network/client.py
@@ -41,14 +41,14 @@ class Connection(threading.Thread):
         version_request = self.main_node.meaning_of_msg['version']
 
         if request_msg_meaning == self.main_node.meaning_of_msg['version']:
-            self.send_version_msg()
+            self.__send_version_msg()
 
         elif request_msg_meaning == self.main_node.meaning_of_msg['get_blocks']:
             self.__answer_get_blocks_msg()
         else:
             pass
 
-    def send_version_msg(self):
+    def __send_version_msg(self):
         height = len(os.listdir('blockchain/blocks')).to_bytes(4, 'big')
         self.send(self.main_node.types['info'], self.main_node.meaning_of_msg['version'], height)
     def __answer_get_blocks_msg(self):
