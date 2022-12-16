e9573b7d828021cc86896f28c1646bbec3cc7db1
eugene <minotavr1712@gmail.com>
2022-12-16 17:09:22 +0300

    .

--- tmp1.py
+++
@@ -1,6 +0,0 @@
-# with open('blockchain/blocks/blk_0000.dat', 'rb') as f:
-#     print(f.read())
-
-from blockchain.blockchain import Block 
-# with open('blockchain/blocks/blk_0001.dat', 'rb') as f:
-print(Block.parseBlock(1))
