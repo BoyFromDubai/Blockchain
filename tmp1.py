ca34bd64d8b7fea920bda937d869ee0d87570b8e
eugene <minotavr1712@gmail.com>
2022-12-16 16:46:00 +0300

    .

--- tmp1.py
+++ tmp1.py
@@ -1,20 +1,6 @@
 with open('blockchain/blocks/blk_0000.dat', 'rb') as f:
     print(f.read())
 
-# import json
-# from blockchain.blockchain import *
-# print(Block.parseBlock(2))
-
-# with open('tmp.json', 'w') as f:
-#     json.dump(parseBlock(1), f)
-
-# with open('blockchain/blocks/blk_0002.dat', 'rb') as f:
-#     print(getBlockTxs(f.read()))
-# # print(getPropertyDataFromBlock('tx_count', 15))
-# from blockchain.mrkl_tree import MerkleTree
-# import time
-
 from blockchain.blockchain import Block 
 # with open('blockchain/blocks/blk_0001.dat', 'rb') as f:
 print(Block.parseBlock(1))
-print(Block.parseBlockDigest(1))
