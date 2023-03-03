ed72d259d4d47cbae172baf9a901eb435a474879
eugene <minotavr1712@gmail.com>
2023-03-03 03:30:08 +0300

    Peers are connecting to each other [DEBUG]

--- debug_proj_conf.sh
+++ debug_proj_conf.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 comment=${1:-'.'}
+FILE=prev_comment.txt
+if ! test -f "$FILE"
+then
+    touch $FILE
+fi
 
 prev_comment=$(cat prev_comment.txt)
-if [[$prev_comment -eq $comment]]
+
+echo $comment > $FILE
+
+if [[ "$prev_comment" == "$comment" ]]
 then
     comment="${comment} [DEBUG]"
 fi
 
+echo $comment
 
 git add .
 git commit -m "$comment"
